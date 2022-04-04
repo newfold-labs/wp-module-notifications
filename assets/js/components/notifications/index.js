2a1564e00566c5aa7423001af4583f3b5c21531f
Evan Mullins <evanrm@gmail.com>
2022-04-04 15:17:22 -0700

    use Notification component and attach listeners which in turn hit proper endpoint

--- assets/js/components/notifications/index.js
+++ assets/js/components/notifications/index.js
@@ -13,20 +13,10 @@
  *              pages: (string) 'pagename' || (Array) [ 'page1', 'page2' ]
  *          }
  *      ],
- *      content: (html) HTML_CONTENT - escaped
+ *      content: (html) HTML_CONTENT - (escaped)
  *  },
  *  ...
  * ]
- * 
- * TODO:
- * [x] Load notifications from api
- * [x] Filter expired notificaitons
- * [x] Filter notifications for context
- * [x] Filter notifications for pages
- * [ ] Dismiss listeners - close
- * [ ] Click listeners - open link/button
- * [ ] Click tracking sendEvent
- * [ ] Trigger to check for more notifications?
  * 
  * @param {*} props 
  * @returns 
@@ -38,7 +28,7 @@ const Notifications = (props) => {
     // on mount load all notifications from module api
     useEffect(() => {
         props.apiFetch( {
-            url: props.resturl + '/newfold-notifications/v1/notifications&context=wp-admin-prime'
+            url: `${props.resturl}/newfold-notifications/v1/notifications&context=wp-admin-prime`
         }).then( ( response ) => {
             setAllNotifications(response);
 		});
@@ -146,16 +136,151 @@ const Notifications = (props) => {
     return (
         <div className={props.classnames('newfold-notifications-wrapper')}>
             {activeMotifications.map(notification => (
-                <div 
-                    id={'notification-' + notification.id }
-                    data-id={notification.id}
-                    className='newfold-notification'
-                    dangerouslySetInnerHTML={ {__html: notification.content} }
+                <Notification 
+                    id={notification.id} 
+                    content={notification.content}
+                    resturl={props.resturl}
+                    apiFetch={props.apiFetch}
                 />
             ))}
         </div>
     )
 
+};
+
+const Notification = ({ id, content, ...props }) => {
+
+    const onClose = ( event ) => {
+        event.preventDefault();
+        if ( event.keycode && ENTER !== event.keycode ) {
+			return;
+		}
+
+        const noticeContainer = document.querySelector('[data-id="' + id +'"]');
+        if ( noticeContainer ) {
+            props.apiFetch( {
+                url: `${props.resturl}/newfold-notifications/v1/notifications/${id}`,
+                method: 'DELETE'
+            }).then( ( response ) => {
+                noticeContainer.remove();
+                console.log(response);
+            });
+        }
+    }
+
+    /**
+     * Send events to the WP REST API
+     *
+     * @param {Object} event The event data to be tracked.
+     */
+    const sendEvent = (event)  => {
+        event.data = event.data || {};
+        event.data.page = window.location.href;
+        props.apiFetch({
+            path: `${props.resturl}/newfold-data/v1/events/`,
+            method: 'POST', 
+            data: event
+        });
+    }
+
+    const onButtonNavigate = ( event ) => {
+        if ( event.keycode && ENTER !== event.keycode ) {
+			return;
+		}
+        sendEvent({
+            action: 'newfold-notification-click',
+            data: {
+                element: 'button',
+                label: event.target.innerText,
+                notificationId: id,
+            }
+        })
+    }
+
+    const onAnchorNavigate = ( event ) => {
+        if ( event.keycode && ENTER !== event.keycode ) {
+			return;
+		}
+        sendEvent({
+            action: 'newfold-notification-click',
+            data: {
+                element: 'a',
+                href: event.target.getAttribute('href'),
+                label: event.target.innerText,
+                notificationId: id,
+            }
+        })
+    }
+
+    useEffect(() => {
+        const noticeContainer   = document.querySelector('[data-id="' + id +'"]');
+        const noticeCloser      = noticeContainer.querySelector('[data-action="close"]');
+        const noticeButtons     = Array.from(noticeContainer.querySelectorAll('button'));
+        const noticeAnchors     = Array.from(noticeContainer.querySelectorAll('a'));
+
+        if (noticeButtons.length) {
+            noticeButtons.forEach(
+                button => {
+                    if (button.getAttribute('data-action') !== 'close') {
+                        button.addEventListener('click', onButtonNavigate);
+                        button.addEventListener('onkeydown', onButtonNavigate);
+                    }
+                }
+            )
+        }
+
+        if (noticeAnchors.length) {
+            noticeAnchors.forEach(
+                link => {
+                    if (link.getAttribute('data-action') !== 'close') {
+                        link.addEventListener('click', onAnchorNavigate);
+                        link.addEventListener('onkeydown', onAnchorNavigate);
+                    }
+                }
+            )
+        }
+
+        if (noticeCloser) {
+            noticeCloser.addEventListener('click', onClose);
+            noticeCloser.addEventListener('onkeydown', onClose);
+        }
+        
+        return () => {
+            if (noticeButtons.length) {
+                noticeButtons.forEach(
+					button => {
+						if (button.getAttribute('data-action') !== 'close') {
+                            button.removeEventListener('click', onButtonNavigate);
+                            button.removeEventListener('onkeydown', onButtonNavigate);
+						}
+					}
+				)
+            }
+            if (noticeAnchors.length) {
+				noticeAnchors.forEach(
+					link => {
+						if (link.getAttribute('data-action') !== 'close') {
+                            link.removeEventListener('click', onAnchorNavigate);
+                            link.removeEventListener('onkeydown', onAnchorNavigate);
+						}
+					}
+				)
+            }
+            if (noticeCloser) {
+                noticeCloser.removeEventListener('click', onClose);
+                noticeCloser.removeEventListener('onkeydown', onClose);
+            }
+        }
+    }, [id]);
+
+    return (
+        <div 
+        id={'notification-' + id }
+        data-id={id}
+        className='newfold-notification'
+        dangerouslySetInnerHTML={ {__html: content} }
+        />
+    );
 };
 
 export default Notifications;
