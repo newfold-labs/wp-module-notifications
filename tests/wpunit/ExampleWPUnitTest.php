9b81b3162856ed763da997ac644a93f9e10dce58
Carlos Rodriguez Brito <crodriguezbrito@gmail.com>
2025-04-21 15:32:29 +0100

    PHPUnit scaffolding

---
+++ tests/wpunit/ExampleWPUnitTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace NewfoldLabs\WP\Module\Notifications;
+
+/**
+ *  Example Unit test.
+ *
+ * @coversDefaultClass \NewfoldLabs\WP\Module\Notifications\Example
+ */
+class ExampleWPUnitTest extends \lucatume\WPBrowser\TestCase\WPTestCase {
+
+}
